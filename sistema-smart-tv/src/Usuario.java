4d52e652008edbd540aaf91978390275cbcf3de5
MardenDuarte <marden1981@gmail.com>
2024-05-04 14:10:27 -0300

    smart tv concluido

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -4,12 +4,28 @@ public class Usuario {
 
     SmartTv smartTv = new SmartTv();
 
-    System.out.println("TV Ligada ?" + smartTv.ligada);
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.aumentarVolume();
+
     System.out.println("Canal Atual ?" + smartTv.canal);
-    System.out.println("Volume Atual ?" + smartTv.volume);
+    smartTv.mudarCanal(13);
+        System.out.println("Canal Atual ?" + smartTv.canal);
+        
+
+        System.out.println("Volume autal : " + smartTv.volume);
+
+
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("Canal Atual ?" + smartTv.canal);
+        System.out.println("Volume Atual ?" + smartTv.volume);
     
-    smartTv.ligar();
-    System.out.println("Novo Status -> TV Ligada ?" + smartTv.ligada);
+    smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ?" + smartTv.ligada);
+
+    smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ?" + smartTv.ligada);
 
     }
 }
