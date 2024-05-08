e6380c7f25300201ac89e35096e5c329823200fd
MardenDuarte <marden1981@gmail.com>
2024-05-08 09:44:09 -0300

    codigo com melhoria de opção de erro para reenserir os dados.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -10,9 +10,14 @@ public class ContaTerminal {
 
         //Exibir a mensagem conta criada
 
+        
+
         System.out.println("Seja bem vindo ao Banco TANA (Teu Atraso Nossa Alegria)");
 
         Scanner scanner = new Scanner(System.in);
+
+        boolean contaCerta = false;
+        while(contaCerta == false){
 
         System.out.println("Digite seu Nome: ");
         String nome = scanner.next();
@@ -29,13 +34,21 @@ public class ContaTerminal {
         System.out.println("Informe o valor para depositar: ");
         double saldoConta = scanner.nextDouble();
 
+        System.out.println("Confirma os dados acima");
+        System.out.println ("[1]-Sim, [2]-Não");
+        int simOuNao = scanner.nextInt();
+        System.out.println("Informe novamente os dados, por favor.");
+
+        if(simOuNao == 1){
+            contaCerta = true;
         System.out.println("Cliente: " + nome + " " + sobrenome);
         System.out.println("Número da sua Agência: " + agenciaNome);
         System.out.println("Número da conta: " + numeroConta);
         System.out.println("Meu valor para deposito inicial é R$ " + saldoConta);
 
-        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco TANA, sua agência é " + agenciaNome + ", Conta " + numeroConta + " e seu saldo de R$ " + saldoConta + " já está disponivel para saque.");
-       
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta no TANA, sua agência é " + agenciaNome + ", Conta " + numeroConta + " e seu saldo de R$ " + saldoConta + " já está disponivel para saque.");
+        }
+        }
 
 
         
